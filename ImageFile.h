a009ef81fea16b58659748d0cc985bc8c32fd12e
roeey1 <roeeyls1234@gmail.com>
2015-01-17 18:45:42 +0200

    headers

---
+++ ImageFile.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "File.h"
+using namespace std;
+#define BYTE char
+
+class ImageFile : File
+{
+public:
+	ImageFile(string path, BYTE size, string name, string type);
+	virtual string get_pathFile();
+	virtual BYTE get_sizeOfFile();
+	virtual string get_fileName();
+	virtual string get_typeOfFile();
+	virtual void set_sizeOfFile(BYTE bt);
+	virtual void set_fileName(string newname);
+	virtual void set_typeOfFile(string type);
+protected:
+	string _pathFile;
+	BYTE _sizeOfFiles;
+	string _fileName;
+	string _typeOfFile;
+};
